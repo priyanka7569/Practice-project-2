6f7f58ebb0b54b3a5821af0db729b4792cc84551
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-23 11:00:25 +0530

    modified

---
+++ ConAppReadFile/ConAppReadFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr;
+            try
+            {
+                string fpath = "C:\\Users\\kotap\\OneDrive\\Desktop\\Practice project2\\Student data.txt";
+                sr = new StreamReader(fpath);
+                string text = " ";
+                while ((text = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(text);
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
